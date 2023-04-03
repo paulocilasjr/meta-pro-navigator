9c711a37b9cb914875b2159a5215b47f44021861
Paulo Cilas Morais Lyra Junior <pj_ibm@hotmail.com>
2023-04-02 23:20:15 -0400

    IRON pipeline completed

--- r_api/rest_controller.R
+++ r_api/rest_controller.R
@@ -1,21 +1,51 @@
+library(plumber)
+library(readr)
 #* @apiTitle IRON tool
 #* Echo provided data to normalize
-#* @param data table Positive and data table Negative
-#* @post /metabolomics_glue 
-function(req){
+#* @post /metabolomics_glue
+#* @param file1:file The Positive CSV file to upload
+#* @param file2:file The Negative CSV file to upload
+#* @serializer csv
+function(req, res){
+  # Check if the two files were attached to the request
+  if(is.null(req$body$file1) || is.null(req$body$file1)){
+    res$status <- 400
+    return(list(error = "Both files must be attached to the request"))
+  }
+  folder_path <- "./normalization_pipeline"
+  
+  tryCatch({
+    file_1_name <- req$body$file1$filename
+    file_2_name <- req$body$file2$filename
+    
+    file_path_1 <- file.path(folder_path, file_1_name)
+    file_path_2 <- file.path(folder_path, file_2_name)
+    
+    file1 <- read_csv(req$body$file1$parsed)
+    file2 <- read_csv(req$body$file2$parsed)
+
+    df <- write_csv(file1, file_path_1)
+    df2 <- write_csv(file2, file_path_2)
+  }, error = function(e) {
+    res$status <- 400
+    return(list(error = "Check file format - must be CSV files"))
+  })
     timestamp = Sys.time()
     file_name = gsub(" ","", timestamp)
     
-    df <- read.csv(req$postBody)
-    write.csv(df, "normalization_pipeline/POS.csv")
-    #write.csv(as.data.frame(req$postBody), "normalization_pipeline/POS.csv")
+    try(system(paste("cd normalization_pipeline && perl generate_metabolomics_glue_script.pl", file_1_name, file_2_name, file_name, "> run_me.sh;sh run_me.sh"), intern = TRUE, ignore.stderr = TRUE))
+    
+    print("Done running pipeline")
 
-    #table <- read.csv('normalization_pipeline/POS.csv')
+    log2_file_name <- paste(file_name,"_iron_log2_merged.txt", sep = "", collapse = "")
+    qc_file_name <- paste(file_name,"_sample_qc_table.txt", sep = "", collapse = "")
+    
+    iron_log2 <- readLines(paste("./normalization_pipeline/",log2_file_name, sep = "", collapse = ""))
+    qc_normalize <- readLines(paste("./normalization_pipeline/",qc_file_name, sep = "", collapse = ""))
+    print("Done reading file")
 
-    df
-
-
-    #try(system(stringr::str_interp("cd normalization_pipeline && perl generate_metabolomics_glue_script.pl POS.csv NEG.csv ${file_name} > run_me.sh;sh run_me.sh"), intern = TRUE, ignore.stderr = TRUE))
-    #try(system(stringr::str_interp("cd normalization_pipeline && rm ${file_name}_iron_log2_merged.txt && rm ${file_name}_sample_qc_table.txt")))
-    #return (file_name)
+    try(system(paste("cd normalization_pipeline && rm", log2_file_name, "&& rm", qc_file_name), intern = TRUE, ignore.stderr = TRUE))
+    print("Done removing files")
+    
+    return (data.frame(iron_log2))
 }
